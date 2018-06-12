0da25fa722a2832a5b4ccfe9dfd2cdc4f9f1c488
alejandronf <alejandronf@gmail.com>
2018-06-12 19:49:20 +0200

    socket lib added both client and server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log(`new user conected on ${socket}`);
+  socket.on('disconnect', () => {
+    console.log('user was disconnected');
+  });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is up on port ${port}`);
 });
